7a43944606618d227e82237be87376572c4445bf
kwabz <95098725+kwabz@users.noreply.github.com>
2022-12-26 22:39:28 +0000

    day 0.5

--- App.js
+++ App.js
@@ -1,20 +1,123 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import {
+  NativeBaseProvider,
+  Box,
+  HStack,
+  CheckIcon,
+  Text,
+  useColorMode,
+  View,
+  extendTheme,
+  VStack,
+  Badge,
+} from "native-base";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import FLTest from "./component/FLTest";
 
-export default function App() {
+// Define the config
+const config = {
+  useSystemColorMode: true,
+  // initialColorMode: "dark",
+};
+
+// extend the theme
+const customTheme = extendTheme({ config });
+
+function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <Box style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Home Screen</Text>
+    </Box>
+  );
+}
+
+function DetailsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Details Screen</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function TestScreen() {
+  let name = "Francis";
+  const { colorMode, toggleColorMode } = useColorMode();
+  return (
+    <VStack space={5}
+      bg={colorMode == "dark" && "#202223"}
+      style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+    >
+      <VStack space={2}>
+        <HStack space={3}>
+          <HStack space={2}>
+            <CheckIcon size="5" mt="0.5" color="emerald.500" />
+            <Text>{name}</Text>
+          </HStack>{" "}
+          <HStack space={2}>
+            <Badge colorScheme="success">
+              <Text>Success</Text>
+            </Badge>{" "}
+            <Badge colorScheme="success">
+              <Text>Success</Text>
+            </Badge>{" "}
+          </HStack>{" "}
+        </HStack>
+        <HStack space={2}>
+          <CheckIcon size="5" mt="0.5" color="emerald.500" />
+          <Text>{name}</Text>
+        </HStack>
+        <HStack space={3}>
+          <HStack space={1}>
+            <Text bold>2,400</Text>
+            <Text>posts</Text>
+          </HStack>
+          <HStack space={1}>
+            <Text bold>2,400</Text>
+            <Text>posts</Text>
+          </HStack>
+          <HStack space={1}>
+            <Text bold>2,400</Text>
+            <Text>posts</Text>
+          </HStack>
+        </HStack>
+        <VStack>
+          <Text bold>Number1#Veeyeahh</Text>
+          <Text>For business opportunities: veelee.only</Text>
+        </VStack>
+      </VStack>
+      <HStack>
+        <FLTest/>
+      </HStack>
+    </VStack>
+  );
+}
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NativeBaseProvider theme={customTheme}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Test">
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Details" component={DetailsScreen} />
+          <Stack.Screen
+            options={{
+              headerStyle: {
+                backgroundColor: "#f4511e",
+              },
+              headerTintColor: "#fff",
+              headerTitleStyle: {
+                fontWeight: "bold",
+              },
+            }}
+            name="Test"
+            component={TestScreen}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </NativeBaseProvider>
+  );
+}
+
+export default App;
